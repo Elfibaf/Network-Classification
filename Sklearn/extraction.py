d492266421fc065958a2a6e5a44cea42310ed128
Fabien <fabien.robin@hotmail.fr>
2016-06-07 12:19:49 +0100

    extraction.py

--- Sklearn/extraction.py
+++ Sklearn/extraction.py
@@ -30,12 +30,30 @@ def load_dataset(filename):
     labels = []
     for row in barray:
         labels.append(row.pop())
+        row.pop()
     nparray = np.array(barray)
     labels = np.array(labels)
+
+
+    #Conversion of string attributes to float
+    nparray = nparray.T
+    for i in range(len(nparray[3])):
+        if nparray[3][i] == 'TCP':
+            nparray[3][i] = 0
+        elif nparray[3][i] == 'UDP':
+            nparray[3][i] = 1
+
+    for i in range(len(nparray[4])):
+        nparray[4][i] = ''.join(nparray[4][i].split('.'))
+        nparray[6][i] = ''.join(nparray[6][i].split('.'))
+
+    nparray = nparray.T
+    
 
     return DataSet(nparray,labels)
 
 
-foo = load_dataset('../Data/Capture_Port.arff')
-print(foo.features, foo.labels, foo.nb_examples)
+#foo = load_dataset('../Data/Capture_Port.arff')
+#print(foo.features, foo.labels, foo.nb_examples)
+
 
