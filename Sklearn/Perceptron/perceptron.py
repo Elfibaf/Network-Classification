efddea7d18e29e0c8b852cd1ee6bf1f40b93f37b
mehdi14000 <mehdi.crozes33@gmail.com>
2016-06-08 16:47:00 +0200

    Modifs Perceptron

--- Sklearn/Perceptron/perceptron.py
+++ Sklearn/Perceptron/perceptron.py
@@ -1,7 +1,7 @@
 # Authors : Mehdi Crozes and Fabien Robin
-# Date : June 7th 2016
+# Date : June 8th 2016
 
-# GaussianNB for ARFF traffic network file 
+# Perceptron via SGDClassifier for ARFF traffic network file 
 
 import arff
 import numpy as np
@@ -9,10 +9,10 @@ import matplotlib.pyplot as py
 import time
 
 from extraction import *
-from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
 from sklearn.cross_validation import train_test_split
 from sklearn.metrics import confusion_matrix
-from sklearn import preprocessing
+from sklearn.preprocessing import MinMaxScaler
 
 # Step 1 : Import Arff file
 
@@ -31,19 +31,27 @@ feature_train,feature_test,label_train,label_test = train_test_split(arff_file.f
 feature_train_float = feature_train.astype(np.float)
 feature_test_float = feature_test.astype(np.float)
 
-t0=time.time()
-clf = GaussianNB()
-label_pred = clf.fit(feature_train_float,label_train).predict(feature_test_float)
-print "Training and predicting time ",round(time.time()-t0,3),"s"
-print "Number of Classes :",len (clf.classes_)
+scaler = MinMaxScaler(copy='false')
+feature_train_rescaled =scaler.fit_transform(feature_train_float)
+feature_test_rescaled=scaler.fit_transform(feature_test_float) 
+
+print feature_train_rescaled,feature_test_rescaled
+#t0=time.time()
+#clf = Perceptron(n_iter=1000)
+#clf.fit(feature_train_float,label_train)
+#print "Training time ",round(time.time()-t0,3),"s"
+#print "Number of Classes :",len (clf.classes_)
+
+#label_pred = clf.fit(feature_train_float,label_train).predict(feature_test_float)
 
 # Step 4 : Generate Confusion Matrix
 
-confusion_matrix = confusion_matrix(label_test,label_pred)
-print confusion_matrix
+#confusion_matrix = confusion_matrix(label_test,label_pred)
+#print confusion_matrix
 
 # Step 5 : Accuracy 
 
-score = clf.score(feature_test_float,label_test)
-print "Test Accuracy :",score 
+#score = clf.score(feature_test_float,label_test)
+#print "Test Accuracy :",score 
+
 
