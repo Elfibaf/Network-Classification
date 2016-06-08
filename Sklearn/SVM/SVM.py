fc777fb558be7aff95b113a2581d692ce2779db8
Fabien <fabien.robin@hotmail.fr>
2016-06-08 09:49:59 +0100

    SVM

---
+++ Sklearn/SVM/SVM.py
@@ -0,0 +1,41 @@
+import re
+import numpy as np
+import os
+import arff
+from sklearn import svm
+import extraction
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import normalize
+from sklearn.grid_search import GridSearchCV
+
+
+
+
+
+data = extraction.load_dataset('../Data/Capture_Port.arff')
+
+#data.features = normalize(data.features)
+#print(data.features)
+
+feature_train,feature_test,label_train,label_test = train_test_split(data.features,data.labels,test_size=0.25,random_state=42)
+
+# Searching for best parameters for the classifier
+param_grid = [
+  {'C': [1, 10, 100, 1000], 'gamma': np.logspace(-20,0,num=21), 'kernel': ['rbf']},
+ ]
+
+model = GridSearchCV(svm.SVC(C=1), param_grid)
+
+X = feature_train
+Y = label_train
+
+
+model.fit(X, Y)
+
+print("Best parameters : ", model.best_params_)
+
+print(model.score(X, Y))
+
+predicted = model.predict(feature_test)
+print(model.score(feature_test, label_test))
+print(predicted)
