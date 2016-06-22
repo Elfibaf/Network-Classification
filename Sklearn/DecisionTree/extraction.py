39fa39b8f57450ab832feeb777ff803d1e967b8b
Fabien <fabien.robin@hotmail.fr>
2016-06-22 14:12:08 +0100

    .

--- Sklearn/DecisionTree/extraction.py
+++ Sklearn/DecisionTree/extraction.py
@@ -35,7 +35,7 @@ def load_dataset(filename):
     labels = np.array(labels)
 
 
-    #Conversion of string attributes to float
+    """#Conversion of string attributes to float
     nparray = nparray.T
     for i in range(len(nparray[3])):
         if nparray[3][i] == 'TCP':
@@ -47,7 +47,7 @@ def load_dataset(filename):
         nparray[4][i] = ''.join(nparray[4][i].split('.'))
         nparray[6][i] = ''.join(nparray[6][i].split('.'))
 
-    nparray = nparray.T
+    nparray = nparray.T"""
     
 
     return DataSet(nparray,labels)
