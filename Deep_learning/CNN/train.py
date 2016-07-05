774cf4573b813e0d7b33aa797bcf6555c0741be8
Fabien <fabien.robin@hotmail.fr>
2016-07-05 11:04:49 +0100

    merge

--- Deep_learning/CNN/train.py
+++ Deep_learning/CNN/train.py
@@ -14,7 +14,7 @@ from extraction import load_dataset_info, save_obj
 # ==================================================
 
 # Model Hyperparameters
-tf.flags.DEFINE_integer("embedding_dim", 256, "Dimensionality of character embedding (default: 128)")
+tf.flags.DEFINE_integer("embedding_dim", 128, "Dimensionality of character embedding (default: 128)")
 tf.flags.DEFINE_string("filter_sizes", "3,4,5", "Comma-separated filter sizes (default: '3,4,5')")
 tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (default: 128)")
 tf.flags.DEFINE_float("dropout_keep_prob", 0.5, "Dropout keep probability (default: 0.5)")
@@ -42,7 +42,7 @@ print("")
 
 # Load data
 print("Loading data...")
-Data = load_dataset_info("../../../Data/Barcelona/packets_all.info", True)
+Data = load_dataset_info("../../../Data/Barcelona/packets_all.info", False)
 x_text, y = Data.features, Data.labels
 
 # Build vocabulary
