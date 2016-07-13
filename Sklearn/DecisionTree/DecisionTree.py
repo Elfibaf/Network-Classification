b0c410126520b903f86b39afb8e4121dc8b8af4f
Mehdi <mehdi14000>
2016-07-13 13:29:01 +0200

    Modifs avec pylint DecisionTree

--- Sklearn/DecisionTree/DecisionTree.py
+++ Sklearn/DecisionTree/DecisionTree.py
@@ -1,60 +1,64 @@
-# Authors : Mehdi Crozes and Fabien Robin
-# Date : June 7th 2016
-# Update : June 26th 2016
+"""
+Authors : Mehdi Crozes and Fabien Robin
+Date : June 7th 2016
+Update : June 26th 2016
+DecisionTreeClassifier for ARFF traffic network file
+"""
 
-# DecisionTreeClassifier for ARFF traffic network file 
-
-import arff
-import numpy as np
-import matplotlib.pyplot as py
 import time
+import matplotlib.pyplot as py
+import numpy as np
 
-from extraction import *
+from extraction import load_dataset, load_dataset_barcelona, plot_confusion_matrix, kfold_data
 from sklearn import tree
-from sklearn.cross_validation import train_test_split
-from sklearn.metrics import confusion_matrix,recall_score,precision_score
-
-# Step 1 : Import Arff file
+from sklearn.metrics import confusion_matrix, recall_score, precision_score
 
 
-#arff_file = load_dataset("../../Data/Caida/features_caida_flowcalc2.arff")
-arff_file = load_dataset_barcelona('../../../Data/Barcelona/packets_all_2.info')
+def main():
 
-print "\tTotal dataset : "
-print "\tNumber of samples:",arff_file.nb_examples
-print "\tNumber of features:",len(arff_file.features[0])
+    """ Step 1: Import Arff file """
 
-# Step 2 : Building training_set and test_set
+    #arff_file = load_dataset("../../Data/Caida/features_caida_flowcalc2.arff")
+    arff_file = load_dataset_barcelona('../../Data/Info_file/packets_all_2.info')
 
-feature_train,feature_test,label_train,label_test = kfold_data(arff_file, 10)
+    print "\tTotal dataset : "
+    print "\tNumber of samples:", arff_file.nb_examples
+    print "\tNumber of features:", len(arff_file.features[0])
 
-# Step 3 : Fitting and training our classifier 
+    """ Step 2: Building training_set and test_set """
 
-clf = tree.DecisionTreeClassifier()
-t0 = time.time()
-clf.fit(feature_train,label_train)
+    feature_train, feature_test, label_train, label_test = kfold_data(arff_file, 10)
 
-print "\tTraining time: ",round(time.time()-t0,3),"s"
-print "\tNumber of Classes :",len (clf.classes_)
+    """Step 3 : Fitting and training """
 
-t1 = time.time()
-label_pred = clf.predict(feature_test)
-print "\tPredicting time: ",round(time.time()-t1,3),"s"
+    clf = tree.DecisionTreeClassifier()
+    t0 = time.time()
+    clf.fit(feature_train, label_train)
 
-# Step 4 : Precision_score and recall_score + plot confusion_matrix
+    print "\tTraining time: ", round(time.time()-t0, 3), "s"
+    print "\tNumber of Classes :", len(clf.classes_)
 
-print "\tPrecision :",precision_score(label_test,label_pred,average='micro')
-print "\tRecall :",recall_score(label_test,label_pred,average='micro')
+    t1 = time.time()
+    label_pred = clf.predict(feature_test)
+    print "\tPredicting time: ", round(time.time()-t1, 3), "s"
 
-cm = confusion_matrix(label_test,label_pred)
-cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-py.figure()
-plot_confusion_matrix(cm_normalized,clf)
-py.show()
+    """Step 4 : Precision_score,recall_score + plot confusion_matrix"""
 
-# Step 5 : Test and Train Accuracy 
+    print "\tPrecision :", precision_score(label_test, label_pred, average='micro')
+    print "\tRecall :", recall_score(label_test, label_pred, average='micro')
 
-score = clf.score(feature_test,label_test)
-score2 = clf.score(feature_train,label_train)
-print "\tTest Accuracy :",score 
-print "\tTrain Accuracy:",score2
+    cm = confusion_matrix(label_test, label_pred)
+    cm_normalized = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    py.figure()
+    plot_confusion_matrix(cm_normalized, clf)
+    py.show()
+
+    """Step 5 : Test and Train Accuracy"""
+
+    score = clf.score(feature_test, label_test)
+    score2 = clf.score(feature_train, label_train)
+    print "\tTest Accuracy :", score
+    print "\tTrain Accuracy:", score2
+
+if __name__ == '__main__':
+    main()
--- Sklearn/DecisionTree/DecisionTree_FeatureSelection.py
+++ Sklearn/DecisionTree/DecisionTree_FeatureSelection.py
@@ -1,65 +1,68 @@
-# Authors : Mehdi Crozes and Fabien Robin
-# Date : June 7th 2016
+"""
+Authors : Mehdi Crozes and Fabien Robin
+Date : June 7th 2016
+DecisionTreeClassifier for ARFF traffic network file
+"""
 
-# DecisionTreeClassifier for ARFF traffic network file 
-
-import arff
-import numpy as np
-import matplotlib.pyplot as py
 import time
+import numpy as np
 
-from extraction import *
+from extraction import load_dataset, load_dataset_barcelona
 from sklearn import tree
 from sklearn.cross_validation import train_test_split
-from sklearn.metrics import confusion_matrix,recall_score,precision_score
-from sklearn.feature_selection import SelectKBest
-from sklearn.feature_selection import chi2, f_classif
-
-# Step 1 : Import Arff file
-
-arff_file = load_dataset("../../../Data/Caida/Features_flowcalc/features_all_time.arff")
-print "\tTotal dataset : "
-print "\tNumber of samples:",arff_file.nb_examples
-print "\tNumber of features:",len(arff_file.features[0])
-
-# Step 2 : Building training_set and test_set
-
-selector = SelectKBest(f_classif, k=4)
-features = arff_file.features.astype(np.float)
-new_feat = selector.fit_transform(features, arff_file.labels)
-feature_train_float,feature_test_float,label_train,label_test = train_test_split(new_feat,arff_file.labels,test_size=0.25,random_state=42)
-
-print("Best scores : ")
-print(selector.scores_)
-
-# Step 3 : Fitting and training our classifier 
-
-#feature_train_float = feature_train.astype(np.float)
-#feature_test_float = feature_test.astype(np.float)
-
-print(feature_train_float[1])
-
-clf = tree.DecisionTreeClassifier()
-
-t0 = time.time()
-clf.fit(feature_train_float,label_train)
-
-print "\tTraining time: ",round(time.time()-t0,3),"s"
-print "\tNumber of Classes :",len (clf.classes_)
-
-t1 = time.time()
-label_pred = clf.predict(feature_test_float)
-print "\tPredicting time: ",round(time.time()-t1,3),"s"
-
-# Step 4 : Precision_score and recall_score
-
-print "\tPrecision :",precision_score(label_test,label_pred,average='micro')
-print "\tRecall :",recall_score(label_test,label_pred,average='micro')
+from sklearn.metrics import recall_score, precision_score
+from sklearn.feature_selection import SelectKBest, chi2, f_classif
 
 
-# Step 5 : Accuracy 
+def main():
 
-score = clf.score(feature_test_float,label_test)
-score2 = clf.score(feature_train_float,label_train)
-print "\tTest Accuracy :",score 
-print "\tTeain Accuracy:",score2
+    # Step 1 : Import Arff file
+
+    arff_file = load_dataset("../../Data/Caida/Features_flowcalc/features_all.arff")
+    print "\tTotal dataset : "
+    print "\tNumber of samples:", arff_file.nb_examples
+    print "\tNumber of features:", len(arff_file.features[0])
+
+    # Step 2 : Building training_set and test_set
+
+    selector = SelectKBest(f_classif, k=4)
+    features = arff_file.features.astype(np.float)
+    new_feat = selector.fit_transform(features, arff_file.labels)
+    feature_train_float, feature_test_float, label_train, label_test = train_test_split(new_feat, arff_file.labels, test_size=0.25, random_state=42)
+
+    print("Best scores : ")
+    print(selector.scores_)
+
+    # Step 3 : Fitting and training our classifier 
+
+    #feature_train_float = feature_train.astype(np.float)
+    #feature_test_float = feature_test.astype(np.float)
+
+    print(feature_train_float[1])
+
+    clf = tree.DecisionTreeClassifier()
+    t0 = time.time()
+    clf.fit(feature_train_float, label_train)
+
+    print "\tTraining time: ", round(time.time()-t0, 3), "s"
+    print "\tNumber of Classes :", len(clf.classes_)
+
+    t1 = time.time()
+    label_pred = clf.predict(feature_test_float)
+    print "\tPredicting time: ", round(time.time()-t1, 3), "s"
+
+    # Step 4 : Precision_score and recall_score
+
+    print "\tPrecision :", precision_score(label_test, label_pred, average='micro')
+    print "\tRecall :", recall_score(label_test, label_pred, average='micro')
+
+
+    # Step 5 : Accuracy
+
+    score = clf.score(feature_test_float, label_test)
+    score2 = clf.score(feature_train_float, label_train)
+    print "\tTest Accuracy :", score
+    print "\tTeain Accuracy:", score2
+
+if __name__ == '__main__':
+    main()
